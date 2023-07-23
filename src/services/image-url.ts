0fbc890278534387f7dd172fca59d2b176a4e990
Tanvir Imam Mitul <blackpanther141532@gmail.com>
2023-07-24 01:03:52 +0600

    Handle games with no image

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,5 +1,7 @@
+ import noImage from "../assets/no-image-placeholder-6f3882e0.webp"
+ 
  const getCropedImageUrl = (url: string) =>{
- if(url === null) return ""
+ if(url === null) return noImage
     const target = 'media/'
     const index = url.indexOf(target) + target.length
     const newUrl = url.slice(0,index) + 'crop/600/400/' + url.slice(index)
